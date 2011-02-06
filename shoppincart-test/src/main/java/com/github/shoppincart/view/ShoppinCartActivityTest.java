5dc792f529d5f564849a86df519f7c7e007b75c2
youribonnaffe <youribm@gmail.com>
2011-02-06 16:47:19 +0100

    new structure for integration tests

---
+++ shoppincart-test/src/main/java/com/github/shoppincart/view/ShoppinCartActivityTest.java
@@ -0,0 +1,9 @@
+package com.github.shoppincart.view;
+
+public class ShoppinCartActivityTest extends android.test.ActivityInstrumentationTestCase2<ShoppinCartActivity> {
+
+	public ShoppinCartActivityTest() {
+		super(ShoppinCartActivity.class);
+	}
+
+}
